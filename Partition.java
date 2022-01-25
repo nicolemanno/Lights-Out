fe8f173aa57257dec2416f8ebabf2adcbcd82c90
nicolemanno <93093054+nicolemanno@users.noreply.github.com>
2022-01-24 22:14:25 -0500

    Update Partition.java

--- Partition.java
+++ Partition.java
@@ -1,103 +1,67 @@
-import java.util.ArrayList;
+import java.util.ArrayList;	
+import java.util.Iterator;
 
 public class Partition {
 
-	int[] dp = new int[88];
-	
+	int[] dp = new int[200];
+
 	/**
 	 * 
 	 * @param idx - index
 	 * @return innerList for bigList
 	 */
-	public ArrayList<Integer> addToList(int idx){
-		
+	public ArrayList<Integer> addToList(int idx) {
+
 		ArrayList<Integer> innerList = new ArrayList<>();
-		for(int i=1; i<idx; i++) {
+		for (int i = 1; i < idx; i++) {
 			innerList.add(dp[i]);
 		}
 		return innerList;
-		
+
 	}
-	
+
 	/**
-	 * @param n - number partitioning
+	 * @param n       - number partitioning
 	 * @param maxVal
 	 * @param idx
 	 * @param count
 	 * @param bigList
 	 * @return bigList containing all partitions for n
 	 */
-	public ArrayList<ArrayList<Integer>> uniquePartitions(int n, int maxVal, int idx, int count, ArrayList<ArrayList<Integer>> bigList ){
+	public ArrayList<ArrayList<Integer>> uniquePartitions(int n, int maxVal, int idx, int count,
+			ArrayList<ArrayList<Integer>> bigList) {
 
-		if(n==0) {
+		if (n == 0) {
 			bigList.add(addToList(idx));
 			count++;
 		}
-		for(int i = maxVal; i>=1; i--) {
-			if(i>n) {
+		for (int i = maxVal; i >= 1; i--) {
+			if (i > n) {
 				continue;
-			}
-			else if(i <=n) {
-				dp[idx]=i;
-				uniquePartitions(n-i, i, idx+1, count, bigList);
+			} else if (i <= n) {
+				dp[idx] = i;
+				uniquePartitions(n - i, i, idx + 1, count, bigList);
 			}
 		}
 		return bigList;
-		
-	}
-	/**
-	 * @param n - number of nodes
-	 * @param k 
-	 * @return partition of n with n-k parts of size 1
-	 */
-	public ArrayList<ArrayList<Integer>> makePartitionsFromK(int n, int k){
-		
-		ArrayList<ArrayList<Integer>> totalList = new ArrayList<ArrayList<Integer>>();
-		ArrayList<ArrayList<Integer>> kParts = uniquePartitions(k, k, 1, 0, totalList);
-		ArrayList<ArrayList<Integer>> returnList = new ArrayList<>();
-		
-		for(ArrayList<Integer> list: kParts) {
-			if(sizeOne(list)==true) {
-				continue;
-			}
-			else {
-				ArrayList<Integer> allOnes = partitionOfAllOnes(n-k);
-				for(int i: list) {
-					allOnes.add(i);
-				}
-				returnList.add(allOnes);
-				
-			}
-		}
-		
-		return returnList;
-	}
-	
-	/**
-	 * @param n
-	 * @return partition of n with n parts of size 1
-	 */
-	public ArrayList<Integer> partitionOfAllOnes(int n){
-		ArrayList<Integer> list = new ArrayList<>();
-		for(int i=0; i<n; i++) {
-			list.add(1);
-		}
-		return list;
+
 	}
 
 	/**
-	 * @param list
-	 * @return true if there exists parts of size 1 in list
+	 * @param k - number partitioning
+	 * @return partitionsOfK - partitions of k with no parts of size 1
 	 */
-	public boolean sizeOne(ArrayList<Integer> list) {
-		boolean existenceOfOnes = false;
-		for(int i=0; i<list.size(); i++) {
-			if(list.get(i)==1) {
-				existenceOfOnes= true;
-				break;
+	public ArrayList<ArrayList<Integer>> partitionWithNoOnes(int k) {
+		ArrayList<ArrayList<Integer>> partitionList = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<Integer>> partitionsOfK = uniquePartitions(k, k, 1, 0, partitionList);
+
+		for (Iterator<ArrayList<Integer>> it = partitionsOfK.iterator(); it.hasNext();) {
+			ArrayList<Integer> part = it.next();
+			if (part.contains(1)) {
+				it.remove();
 			}
 		}
-		return existenceOfOnes;
+		return partitionsOfK;
 	}
 
 }
